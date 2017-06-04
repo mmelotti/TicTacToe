dadd7b5a118297a80c050f2891ccc616e87cac37
Sanne Peters <sanne@sanstream.nl>
2017-06-04 16:29:50 +0200

    Added styling for the page header.

--- web-app/src/js/kitchen-sink.js
+++ web-app/src/js/kitchen-sink.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import AllComponents from './components/AllComponents';
 
 document.addEventListener('DOMContentLoaded', () => {
-  var body = document.getElementsByTagName('body')[0];
+  var main = document.getElementsByTagName('main')[0];
   console.log('test');
-  ReactDOM.render(<AllComponents/>, body);
+  ReactDOM.render(<AllComponents/>, main);
 }, false);
