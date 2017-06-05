227c9c2776428f0abf1ef342eaedd2d75d5a8313
Sanne Peters <sanne@sanstream.nl>
2017-06-05 10:43:59 +0200

    Extended the kitchensink

--- web-app/src/js/components/AllComponents.js
+++ web-app/src/js/components/AllComponents.js
@@ -10,25 +10,50 @@ export default class AllComponents extends React.Component {
 
     this.state = {
       list :[
-        (<Button label="Generic button"/>),
-        (<Button label="player 1's button" associatedTo="player-1"/>),
-        (<Button label="Player 2's button" associatedTo="player-2"/>),
-        (<Field value=""/>),
-        (<Field placeholder="player 1's field" associatedTo="player-1"/>),
-        (<Field placeholder="Player 2's field" associatedTo="player-2"/>),
-        (<Label text="This is a label" />),
+        {
+          'name': 'Atoms',
+          'list':[
+            (<Button label="Generic button"/>),
+            (<Button label="player 1's button" associatedTo="player-1"/>),
+            (<Button label="Player 2's button" associatedTo="player-2"/>),
+            (<Field value=""/>),
+            (<Field placeholder="player 1's field" associatedTo="player-1"/>),
+            (<Field placeholder="Player 2's field" associatedTo="player-2"/>),
+            (<Label text="This is a label" />),
+          ]
+        },
+        {
+          'name': 'Molecules',
+          'list':[
+          ]
+        },
+        {
+          'name': 'Organisms',
+          'list':[
+          ]
+        },
       ]
     };
   }
 
   render () {
     const list = this.state.list;
-    return (
-      <ol className="all-components">
-        {list.map((comp,i) => (
-          <li key={i}>{comp}</li>
-        ))}
-      </ol>
+    console.log(list);
+    const renderedList = (
+      <section>
+        {list.map((level, i) => (
+          <article key={i}>
+            <h2>{level.name}:</h2>
+            <ol className="all-components">
+              {level.list.map((comp,i) => (
+                <li key={i}>{comp}</li>
+              ))}
+            </ol>
+          </article>
+          )
+        )}
+      </section>
     );
+    return renderedList;
   }
 }
