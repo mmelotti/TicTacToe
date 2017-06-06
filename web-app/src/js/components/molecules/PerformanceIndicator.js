add1e5097a9e04448a52333aa0b93092ac95e4bc
Sanne Peters <sanne@sanstream.nl>
2017-06-06 11:17:29 +0200

    Added the performance indicator molecule.

--- web-app/src/js/components/molecules/PerformanceIndicator.js
+++ web-app/src/js/components/molecules/PerformanceIndicator.js
@@ -6,7 +6,11 @@ export default class PerformanceIndicator extends React.Component {
 
     this.ticker = null;
     this.initialRating = 5;
-    this.initialTime = 1000;
+    this.initialTime = 500;
+    this.colour = {
+      'player-1': "#f47c68",
+      'player-2': "#4adc69",
+    };
 
     this.state = {
       associatedTo: props.associatedTo,
@@ -14,17 +18,20 @@ export default class PerformanceIndicator extends React.Component {
       remainingTime: this.initialTime, // in seconds
       rating: this.initialRating, // starting rate.
     };
+
+    this.bubbles = Array.from({length: this.initialRating}, (v, i) => i+1);
+    console.log(this.bubbles);
   }
 
   updateRating () {
     this.setState({
-      rating : Math.round(this.initialRating * (this.state.remainingTime / this.initialTime))
+      rating : this.initialRating * (this.state.remainingTime / this.initialTime)
     });
   }
 
   updateTime () {
     this.setState({
-      remainingTime: (this.state.remainingTime - 50)
+      remainingTime: (this.state.remainingTime - 1)
     });
 
     if(this.state.remainingTime === 0) {
@@ -48,10 +55,12 @@ export default class PerformanceIndicator extends React.Component {
   render () {
     const props = this.props;
     const state = this.state;
+    const self = this;
+
     return (
       <figure className="performance-indicator" data-owned-by={props.playerInTurn}>
         <figcaption>Wiph van Winkle</figcaption>
-        <table>
+        <table>1
           <tbody>
             <tr>
               <th>Points</th>
@@ -59,7 +68,26 @@ export default class PerformanceIndicator extends React.Component {
             </tr>
             <tr>
               <td>{state.remainingTime}</td>
-              <td>{state.rating}</td>
+              <td className="rating">
+                <div className="visual">
+                  {self.bubbles.map(function (bubble) {
+                    const ratingRoundedUp = parseInt(state.rating,10) + 1;
+                    const plyClr = self.colour['player-1'];
+                    var style;
+                    if(bubble < ratingRoundedUp) {
+
+                      style = plyClr;
+                    } else if (bubble === ratingRoundedUp) {
+
+                      const perc = (state.rating - parseInt(state.rating, 10)) * 100;
+                      style = `linear-gradient(to right, ${plyClr} 0%,${plyClr} ${perc-10}%,#efefef ${perc}%,#efefef 100%)`;
+                    } else {
+                      style = '#efefef';
+                    }
+                      return (<span key={bubble} className="bubble" style={{background:style}}></span>)
+                  })}
+                </div>
+              </td>
             </tr>
           </tbody>
         </table>
