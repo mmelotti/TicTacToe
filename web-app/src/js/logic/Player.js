0880e0ae64e651bacf98df46f56b2e2189a3af56
Sanne Peters <sanne@sanstream.nl>
2017-06-07 11:17:26 +0200

    Introduced a default name for when no name is filled in.

--- web-app/src/js/logic/Player.js
+++ web-app/src/js/logic/Player.js
@@ -1,7 +1,8 @@
 export default class Player {
   constructor() {
 
-    this.name = null;
+    this._name = null;
+    this._dummyName = this._generateRandomName();
     this.id = null;
   }
 
@@ -11,5 +12,36 @@ export default class Player {
 
   setId (id) {
     this.id = id;
+  }
+
+  set name (name) {
+    this._name = name;
+  }
+
+  get name () {
+    return (this._name ? this._name : this._dummyName);
+  }
+
+  _generateRandomName () {
+    const names = [
+    ["Runny", "Buttercup", "Dinky", "Stinky", "Crusty",
+    "Greasy","Sun", "Cheesypoof", "Lumpy", "Wacky", "Tiny", "Flunky",
+    "Fluffy", "Zippy", "Doofus", "Gobsmacked", "Slimy", "The Great", "Salamander",
+    "Oily", "Burrito", "Bumpy", "Loopy", "Snotty", "Irving", "Egbert"],
+    ["Waffer", "Lilly","Spooky","Sandy", "Fuzzy","Kitty",
+     "Puppy", "Snuggles","Rubber", "Stinky", "Lulu", "Lola", "Sparkle", "Glitter",
+     "Silver", "Golden", "Rainbow", "Cloud", "Rain", "Stormy", "Winky", "Sugar",
+     "Twinkle", "Star", "Halo", "Angel"],
+    ["Snicker", "Buffalo", "Gross", "Bubble", "Sheep",
+     "Corset", "Toilet", "Lizard", "Waffle", "Kumquat", "Burger", "Grasshopper", "Liver",
+     "Gorilla", "Rhino", "Emu", "Pizza", "Toad", "Gerbil", "Pickle", "Tofu",
+    "Chicken", "Potato", "Mole-person", "Lemur", "Cumberbatch"]
+  ];
+
+    return [
+      names[0][Math.floor(Math.random() * names[0].length)],
+      // names[1][Math.floor(Math.random() * names[1].length)],
+      names[2][Math.floor(Math.random() * names[2].length)],
+    ].join(" ");
   }
 }
